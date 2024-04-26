763b1ba5fa105ed681605a793411eee8aaaefa9e
PasqualeGraziano <pasquale.graziano04@gmail.com>
2024-04-26 15:20:27 +0200

    Aggiornamento service ordini_in_arrivo

--- src/main/java/com/example/supermercato/service/OfferteinarrivoServiceImpl.java
+++ src/main/java/com/example/supermercato/service/OfferteinarrivoServiceImpl.java
@@ -3,10 +3,12 @@ package com.example.supermercato.service;
 import com.example.supermercato.dao.OfferteinarrivoDao;
 import com.example.supermercato.model.Offerteinarrivo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class OfferteinarrivoServiceImpl implements OfferteinarrivoService{
 
     @Autowired
