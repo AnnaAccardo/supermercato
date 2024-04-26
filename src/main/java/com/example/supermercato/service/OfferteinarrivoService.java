f48cd601b6b8298620e5f7f23a1e02fd804fd462
AnnaAccardo <anna.accardo.work@gmail.com>
2024-04-26 22:27:19 +0200

    fix attiva offerte in arrivo

--- src/main/java/com/example/supermercato/service/OfferteinarrivoService.java
+++ src/main/java/com/example/supermercato/service/OfferteinarrivoService.java
@@ -1,8 +1,6 @@
 package com.example.supermercato.service;
 
 import com.example.supermercato.model.OfferteInArrivo;
-import com.example.supermercato.model.Prodotto;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDate;
 import java.util.List;
