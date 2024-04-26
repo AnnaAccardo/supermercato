48e95b6eb27acb10d7c96e98c293aa0467110e2a
AndreaAliberti11 <andrea.aliberti.11@gmail.com>
2024-04-26 16:59:21 +0200

    aggiornamento

--- src/main/java/com/example/supermercato/service/OfferteinarrivoService.java
+++ src/main/java/com/example/supermercato/service/OfferteinarrivoService.java
@@ -1,6 +1,8 @@
 package com.example.supermercato.service;
 
 import com.example.supermercato.model.OfferteInArrivo;
+import com.example.supermercato.model.Prodotto;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -8,5 +10,9 @@ public interface OfferteInArrivoService {
 
     OfferteInArrivo getOfferteInArrivoById(int id);
     List<OfferteInArrivo> getOfferteInArrivo();
+
+    void registraOfferte(OfferteInArrivo offerteInArrivo, int idSottoCategoria, int idValoreOfferta, String dataInizio, String dataFine);
+    void cancellaOfferte(int idOfferteInArrivo);
+
 
 }
